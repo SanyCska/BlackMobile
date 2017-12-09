7a6ad834c088a190d5316afb632cdcfcc9340344
SanyCska <alexander2302@mail.ru>
2017-12-10 01:19:48 +0300

    Successfull running on androidgit add --all!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 # from socket import *
 from kivy.uix.floatlayout import FloatLayout
 import socket
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
 #
 # HOST = 'localhost'  # локальный адрес localhost или 127.0.0.1
 #
@@ -15,24 +17,32 @@ import socket
 #
 # tcpCliSock.connect(ADDR)  # установка связи с сервером
 host = "192.168.1.68"
+# host = "localhost"
 port = 3000
 
-s = socket.socket()
-s.connect((host, port))
+
+class CustomDropDown(DropDown):
+    pass
 
 class AddLocationForm(FloatLayout):
 
     def send_data(self, data):
         # tcpCliSock.send(data.encode())  # отправка данных в bytes
         # print(data)
-
+        s = socket.socket()
+        s.connect((host, port))
         # s = socket.socket()
-        encoded_msg = bytes(data, "utf-8")
-        s.send(encoded_msg)
+        # # encoded_msg = bytes(data, "utf-8")
+        # s.send(encoded_msg)
+        s.send(data.encode())
         s.close()
+        del s
 
 class MirrorApp(App):
-    pass
+    dropdown = CustomDropDown()
+    mainbutton = Button(text='Hello', size_hint=(0.4, 0.1), pos_hint =  {"x":0.3, "y":0.8})
+    mainbutton.bind(on_release=dropdown.open)
+    dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
 
 
 
--- server.py
+++ server.py
@@ -26,6 +26,7 @@
 import socket
 
 host = "192.168.1.68"
+# host = "localhost"
 port=3000
 s=socket.socket()
 s.bind((host, port))
@@ -36,7 +37,8 @@ def server():
                 c, addr=s.accept()
                 print("\nconnection successful with "+str(addr)+"\n\n")
                 data=c.recv(1024)
-                decoded_data=data.decode("utf-8")
+                # decoded_data=data.decode("utf-8")
+                decoded_data = data.decode()
                 if not decoded_data:
                         print("connection with "+ str(addr)+ " broken\n")
                 else:
