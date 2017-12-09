b49eeaff588ef2c29cb516ad42ef971e68eded3e
SanyCska <alexander2302@mail.ru>
2017-12-10 02:26:35 +0300

    added sending on server as json.dump

--- main.py
+++ main.py
@@ -4,18 +4,8 @@ from kivy.uix.floatlayout import FloatLayout
 import socket
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
-#
-# HOST = 'localhost'  # локальный адрес localhost или 127.0.0.1
-#
-# PORT = 21110  # порт на котором работает сервер
-#
-# BUFSIZ = 1024
-#
-# ADDR = (HOST, PORT)
-#
-# tcpCliSock = socket(AF_INET, SOCK_STREAM)
-#
-# tcpCliSock.connect(ADDR)  # установка связи с сервером
+import json
+
 host = "192.168.1.68"
 # host = "localhost"
 port = 3000
@@ -25,28 +15,29 @@ class CustomDropDown(DropDown):
     pass
 
 class AddLocationForm(FloatLayout):
-
+    user = ''
     def send_data(self, data):
         # tcpCliSock.send(data.encode())  # отправка данных в bytes
         # print(data)
+
         s = socket.socket()
         s.connect((host, port))
         # s = socket.socket()
         # # encoded_msg = bytes(data, "utf-8")
         # s.send(encoded_msg)
-        s.send(data.encode())
+        b = dict({'destination': self.user, 'text': data})
+        s.send(json.dumps(b).encode())
         s.close()
         del s
+    def set_user(self, data):
+        self.user = data
+
 
 class MirrorApp(App):
-    dropdown = CustomDropDown()
-    mainbutton = Button(text='Hello', size_hint=(0.4, 0.1), pos_hint =  {"x":0.3, "y":0.8})
-    mainbutton.bind(on_release=dropdown.open)
-    dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+    pass
 
 
 
 
 if __name__ == '__main__':
     MirrorApp().run()
-    # tcpCliSock.close()
--- server.py
+++ server.py
@@ -1,28 +1,5 @@
 # # -*- coding: utf-8 -*-
-#
-# from socket import *
-# HOST = '192.168.1.68'  # адрес хоста (сервера) пустой означает использование любого доступного адреса
-# PORT = 21112  # номер порта на котором работает сервер (от 0 до 65525, порты до 1024 зарезервированы для системы, порты TCP и UDP не пересекаются)
-# BUFSIZ = 1024  # размер буфера 1Кбайт
-# ADDR = (HOST, PORT)  # адрес сервера
-# tcpSerSock = socket(AF_INET, SOCK_STREAM)  # создаем сокет сервера
-# tcpSerSock.bind(ADDR)  # связываем сокет с адресом
-# tcpSerSock.listen(5)  # устанавливаем максимальное число клиентов одновременно обслуживаемых
-# while True:  # бесконечный цикл сервера
-#     print('Waiting for client...')
-#
-#     tcpCliSock, addr = tcpSerSock.accept()  # ждем клиента, при соединении .accept() вернет имя сокета клиента и его адрес (создаст временный сокет tcpCliSock)
-#
-#     print('Connected from: {}'.format(addr))
-#
-#     while True:
-#         data = tcpCliSock.recv(BUFSIZ)
-#         data.decode('utf8')
-#         if data:
-#             print(data)
-#
-# tcpSerSock.close()  # закрытие сокета сервера
-
+import json
 import socket
 
 host = "192.168.1.68"
@@ -42,6 +19,6 @@ def server():
                 if not decoded_data:
                         print("connection with "+ str(addr)+ " broken\n")
                 else:
-                        print("-> "+ decoded_data + "\n")
+                        print(json.loads(data.decode()))
 
 server()
