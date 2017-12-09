7eef775dcb86261db0c242786db848fd3c45b327
SanyCska <alexander2302@mail.ru>
2017-12-09 06:10:58 +0300

    trying do client-server connection

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from kivy.app import App
-from socket import *
+# from socket import *
 from kivy.uix.floatlayout import FloatLayout
+import socket
+#
+# HOST = 'localhost'  # локальный адрес localhost или 127.0.0.1
+#
+# PORT = 21110  # порт на котором работает сервер
+#
+# BUFSIZ = 1024
+#
+# ADDR = (HOST, PORT)
+#
+# tcpCliSock = socket(AF_INET, SOCK_STREAM)
+#
+# tcpCliSock.connect(ADDR)  # установка связи с сервером
+host = "192.168.1.68"
+port = 3000
+s = socket.socket()
 
-HOST = 'localhost'  # локальный адрес localhost или 127.0.0.1
-
-PORT = 21110  # порт на котором работает сервер
-
-BUFSIZ = 1024
-
-ADDR = (HOST, PORT)
-
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
-
-tcpCliSock.connect(ADDR)  # установка связи с сервером
 
 class AddLocationForm(FloatLayout):
 
     def send_data(self, data):
-        tcpCliSock.send(data.encode())  # отправка данных в bytes
+        # tcpCliSock.send(data.encode())  # отправка данных в bytes
         # print(data)
+        # s = socket.socket()
+        # s.connect((host, port))
+        s = socket.socket()
+        encoded_msg = bytes(data, "utf-8")
+        s.send(encoded_msg)
+        s.close()
 
 class MirrorApp(App):
     pass
 
 
+
+
 if __name__ == '__main__':
     MirrorApp().run()
-    tcpCliSock.close()
+    # tcpCliSock.close()
--- server.py
+++ server.py
@@ -1,22 +1,45 @@
-from socket import *
-HOST = 'localhost'  # адрес хоста (сервера) пустой означает использование любого доступного адреса
-PORT = 21110  # номер порта на котором работает сервер (от 0 до 65525, порты до 1024 зарезервированы для системы, порты TCP и UDP не пересекаются)
-BUFSIZ = 1024  # размер буфера 1Кбайт
-ADDR = (HOST, PORT)  # адрес сервера
-tcpSerSock = socket(AF_INET, SOCK_STREAM)  # создаем сокет сервера
-tcpSerSock.bind(ADDR)  # связываем сокет с адресом
-tcpSerSock.listen(5)  # устанавливаем максимальное число клиентов одновременно обслуживаемых
-while True:  # бесконечный цикл сервера
-    print('Waiting for client...')
+# # -*- coding: utf-8 -*-
+#
+# from socket import *
+# HOST = '192.168.1.68'  # адрес хоста (сервера) пустой означает использование любого доступного адреса
+# PORT = 21112  # номер порта на котором работает сервер (от 0 до 65525, порты до 1024 зарезервированы для системы, порты TCP и UDP не пересекаются)
+# BUFSIZ = 1024  # размер буфера 1Кбайт
+# ADDR = (HOST, PORT)  # адрес сервера
+# tcpSerSock = socket(AF_INET, SOCK_STREAM)  # создаем сокет сервера
+# tcpSerSock.bind(ADDR)  # связываем сокет с адресом
+# tcpSerSock.listen(5)  # устанавливаем максимальное число клиентов одновременно обслуживаемых
+# while True:  # бесконечный цикл сервера
+#     print('Waiting for client...')
+#
+#     tcpCliSock, addr = tcpSerSock.accept()  # ждем клиента, при соединении .accept() вернет имя сокета клиента и его адрес (создаст временный сокет tcpCliSock)
+#
+#     print('Connected from: {}'.format(addr))
+#
+#     while True:
+#         data = tcpCliSock.recv(BUFSIZ)
+#         data.decode('utf8')
+#         if data:
+#             print(data)
+#
+# tcpSerSock.close()  # закрытие сокета сервера
 
-    tcpCliSock, addr = tcpSerSock.accept()  # ждем клиента, при соединении .accept() вернет имя сокета клиента и его адрес (создаст временный сокет tcpCliSock)
+import socket
 
-    print('Connected from: {}'.format(addr))
+host = "192.168.1.68"
+port=3000
+s=socket.socket()
+s.bind((host, port))
+s.listen(10)
 
-    while True:
-        data = tcpCliSock.recv(BUFSIZ)
-        data.decode('utf8')
-        if data:
-            print(data)
+def server():
+        while True:
+                c, addr=s.accept()
+                print("\nconnection successful with "+str(addr)+"\n\n")
+                data=c.recv(1024)
+                decoded_data=data.decode("utf-8")
+                if not decoded_data:
+                        print("connection with "+ str(addr)+ " broken\n")
+                else:
+                        print("-> "+ decoded_data + "\n")
 
-tcpSerSock.close()  # закрытие сокета сервера
+server()
